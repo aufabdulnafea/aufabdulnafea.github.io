27f0d454d7f2f2855a91e2edd0c22e64854a69ca
aufabdulnafea <auf.abdulnafea@gmail.com>
2023-09-05 01:46:41 +0200

    name and email

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,21 @@
 function App() {
-  return <div>Hello, World 2!</div>;
+  return (
+    <div className='w-screen h-screen bg-slate-950 flex items-center justify-center'>
+      <div>
+        <div className='text-5xl text-slate-400 font-bold py-2'>
+          Abdul Nafea Auf
+        </div>
+        <div className='text-slate-400 text-xl flex items-center'>
+          <div className='text-lg w-10 h-10 flex items-center mr-2 border border-slate-800 justify-center bg-slate-900 rounded-full'>
+            @
+          </div>
+          <a href='mailto:auf.abdulnafea@gmail.com' className='underline'>
+            auf.abdulnafea@gmail.com
+          </a>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
